a266810dea2ea2d624b691a841f492f8ecd98045
David <davidgj1994@gmail.com>
2018-06-18 12:56:18 +0200

    cambiado prototxts

--- Scripts/compute_bn_statistics.py
+++ Scripts/compute_bn_statistics.py
@@ -7,7 +7,7 @@ from argparse import ArgumentParser
 
 
 
-caffe_root = '/SegNet/caffe-segnet/' 			# Change this to the absolute directoy to SegNet Caffe
+caffe_root = '/home/david/projects/caffe-segnet-cudnn5/' 			# Change this to the absolute directoy to SegNet Caffe
 import sys
 sys.path.insert(0, caffe_root + 'python')
 
