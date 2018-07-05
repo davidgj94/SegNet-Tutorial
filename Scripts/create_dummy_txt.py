82d2f4dc12afce8550a432ebb6a739948a22e8cb
David <davgalla@ing.uc3m.es>
2018-07-05 12:11:10 +0200

    python layer y otros cambios

--- Scripts/create_dummy_txt.py
+++ Scripts/create_dummy_txt.py
@@ -4,37 +4,34 @@ from argparse import ArgumentParser
 
 def create_dummt_txt_(base_txt, dummy_imgs_dir):
 
-    dummy_txt_path = '../{}-dummy.txt'.format(base_txt)
-    if os.path.exists(dummy_txt_path):
-        os.remove(dummy_txt_path)
-    os.mknod(dummy_txt_path)
-    #pdb.set_trace()
+    X_txt_path = '../X.txt'.format(base_txt)
+    if os.path.exists(X_txt_path):
+        os.remove(X_txt_path)
+    os.mknod(X_txt_path)
+
+    Y_txt_path = '../Y.txt'.format(base_txt)
+    if os.path.exists(Y_txt_path):
+        os.remove(Y_txt_path)
+    os.mknod(Y_txt_path)
 
     X_dir = os.path.join(dummy_imgs_dir, 'X.png')
     Y_dir = os.path.join(dummy_imgs_dir, 'Y.png')
 
     base_txt_path = '../roads/ROADS/{}.txt'.format(base_txt)
     num_lines = sum(1 for line in open(base_txt_path))
-    #pdb.set_trace()
 
     for i in range(num_lines):
-        with open(dummy_txt_path, 'a') as txt:
-            txt.write('{} {}\n'.format(X_dir, Y_dir))
+        with open(X_txt_path, 'a') as X_txt:
+            X_txt.write('{} {}\n'.format(X_dir, 0))
+        with open(Y_txt_path, 'a') as Y_txt:
+            Y_txt.write('{} {}\n'.format(Y_dir, 0))
 
 
 def make_parser():
     parser = ArgumentParser()
-    parser.add_argument('-train', action='store_true')
-    parser.add_argument('-test', action='store_true')
-    parser.add_argument('-val', action='store_true')
     parser.add_argument('--dummy_dir', type=str, required=True)
     return parser
 
 p = make_parser()
 args = p.parse_args()
-if args.train:
-    create_dummt_txt_('train', args.dummy_dir)
-if args.test:
-    create_dummt_txt_('test', args.dummy_dir)
-if args.val:
-    create_dummt_txt_('val', args.dummy_dir)
+create_dummt_txt_('train', args.dummy_dir)
--- Scripts/start_training.sh
+++ Scripts/start_training.sh
@@ -7,7 +7,7 @@ CAFFE_DIR="${ROOT_DIR}"/caffe-segnet-cudnn5/build/tools
 PYCAFFE_DIR="${ROOT_DIR}"/caffe-segnet-cudnn5/python
 ROADS_DIR="${SEGNET_TUTORIAL_DIR}"/roads/ROADS
 RESULTS_DIR="${SEGNET_TUTORIAL_DIR}"/results
-export PYTHONPATH=$PYTHONPATH:$PYCAFFE_DIR
+export PYTHONPATH=$PYTHONPATH:$PYCAFFE_DIR:$WORK_DIR
 cd $WORK_DIR
 
 
@@ -29,18 +29,16 @@ case ${option} in
 		MODEL_DIR="${SEGNET_TUTORIAL_DIR}"/Models/segnet_balanced
 		;; 
    *)  
-      	echo "`basename ${0}`:usage: [-b bayesian segnet] | [-s segnet] [-sb balanced segnet]" 
-      	exit 1 # Command to come out of the program with status 1
-      	;; 
+      echo "`basename ${0}`:usage: [-b bayesian segnet] | [-s segnet] [-sb balanced segnet]" 
+      exit 1 # Command to come out of the program with status 1
+      ;; 
 esac
 
-echo $MODEL_DIR
-
-exit 1
-
-python create_exp_dirs.py --exp_name "${exp_name}"
+#python create_exp_dirs.py --exp_name "${exp_name}"
 
 "${CAFFE_DIR}"/caffe train -gpu 0 -solver "${MODEL_DIR}"/solver.prototxt -weights "${SEGNET_TUTORIAL_DIR}"/segnet_pascal.caffemodel
+
+exit 1
 
 python compute_bn_statistics.py --train_model "${MODEL_DIR}"/train.prototxt --weights_dir "${SEGNET_TUTORIAL_DIR}"/Models/Training/"${exp_name}" --out_dir "${SEGNET_TUTORIAL_DIR}"/Models/Inference/"${exp_name}"
 
