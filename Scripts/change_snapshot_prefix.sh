0680241ce9b297e08a26484422123e94d79f0a3b
David <davgalla@ing.uc3m.es>
2018-07-18 10:26:33 +0200

    cambia el net en el solver.prototxt

--- Scripts/change_snapshot_prefix.sh
+++ Scripts/change_snapshot_prefix.sh
@@ -2,4 +2,5 @@
 solver_dir=$1
 exp_name=$2
 
+sed -ri "s|net: \"/home/davidgj/projects_v2/SegNet-Tutorial/Models/.*/train.prototxt\"|net: \"${solver_dir}/train.prototxt\"|" "${solver_dir}"/solver.prototxt
 sed -ri "s|(snapshot_prefix: \"/home/davidgj/projects_v2/SegNet-Tutorial/Models/Training/).*(/snapshot\")|\1${exp_name}\2|" "${solver_dir}"/solver.prototxt
