8c183ec5ccf8cc12eaa9e64c136ece9e9b4c9161
David <davgalla@ing.uc3m.es>
2018-09-24 16:40:27 +0200

    falta el reshape

--- Scripts/canard/tests.py
+++ Scripts/canard/tests.py
@@ -1,8 +1,17 @@
-import video_utils
-import numpy
+import utils
+import numpy as np
 import pdb
+from PIL import Image
+from pathlib import Path
 
-reader = video_utils.VideoReader('/home/davidgj/drente/projects_v2/009-APR-20-2-90.MOV', 1)
-succes, img = reader.read()
-pdb.set_trace()
+path = "/home/davidgj/projects_v2/SegNet-Tutorial/canard/train_old"
+p = Path(path)
+for glob in p.glob("*"):
+	img = np.asarray(Image.open('{}/{}'.format(path, glob.parts[-1])))
+	batch = utils.crop_imgs(img)
+	masks_prueba = np.squeeze(batch[:,0,:,:])
+	full_mask_prueba1 = img[:,:,-1]
+	full_mask_prueba2 = utils.get_full_mask(masks_prueba)
+	pdb.set_trace()
+
 print "End"
---
+++ Scripts/canard/utils.py
@@ -0,0 +1,52 @@
+import sys
+import numpy as np
+from PIL import Image, ImageOps
+import cv2
+import pdb
+print(cv2.__version__)
+
+class VideoReader:
+    
+    def __init__(self, video_path, fps):
+        self.vidcap = cv2.VideoCapture(video_path)
+        self.count = 0
+        self.delay_msec = int(1000 * (1 / fps))
+        
+    def read(self):
+        
+        if self.count > 0:
+            self.vidcap.set(cv2.CAP_PROP_POS_MSEC,(self.count * self.delay_msec))    # added this line 
+        success,image = self.vidcap.read()
+        self.count = self.count + 1
+        
+        return success, image
+        
+def crop_imgs(img, crop_width=1024, crop_height=1080, border=(0,4)):
+    
+    img_width, img_height = img.shape[1], img.shape[0]
+    img = Image.fromarray(img)
+
+    img_batch = []
+    for i_index, i in enumerate(np.arange(0,img_width, crop_width)):
+        for j_index, j in enumerate(np.arange(0,img_height, crop_height)):
+            img_aux = ImageOps.expand(img.crop((i,j,i + crop_width,j+crop_height)), border=border, fill='black')
+            img_aux = np.array(img_aux)
+            img_aux = np.transpose(img_aux, (2,0,1))
+            img_aux = img_aux[(2,1,0),:,:]
+            img_batch.append(img_aux)
+
+    return np.array(img_batch)
+
+def get_full_mask(masks, h=2, w=4, border=(0, 4)):
+
+    num_masks = masks.shape[0]
+    crop_width = masks.shape[2] - 2 * border[0]
+    crop_height = masks.shape[1] - 2 * border[1]
+    masks_ = np.zeros((num_masks, crop_height, crop_width))
+
+    for i in range(num_masks):
+        masks_[i,:,:] = masks[i,border[1]:border[1]+crop_height,border[0]:border[0]+crop_width]
+
+    full_mask = masks_.transpose(2,0,1).reshape(h * crop_height, w * crop_width)
+
+    return full_mask
